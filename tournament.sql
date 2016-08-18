04630025fce7c1861cfa418196fca78224e1d87c
Sam Atkins <sam@cubi.io>
2016-08-18 12:55:42 +0100

    feat:	amended database tables and views so that function reportMatch works as intended

--- tournament.sql
+++ tournament.sql
@@ -2,22 +2,18 @@
 --
 
 -- table to store player name and id number (auto assigned via serial type)
--- ok; test with data entries
 CREATE TABLE players (player_id	SERIAL PRIMARY KEY, name TEXT NOT NULL);
 
 -- table to record match results
--- ok
 CREATE TABLE matches (
-	match_id		SERIAL PRIMARY KEY,
-	match_date		DATE,
-	player_id		INTEGER,
-	player_name		TEXT,
-	match_result	TEXT,
-	FOREIGN KEY (player_id) REFERENCES players (player_id)
+	match_id	SERIAL PRIMARY KEY,
+	winner		INTEGER,
+	loser		INTEGER,
+	FOREIGN KEY (winner) REFERENCES players (player_id),
+	FOREIGN KEY (loser) REFERENCES players (player_id)
 	);
 
 -- view: counts all players; 
--- ok; test with data entries
 CREATE VIEW count_players AS
 	SELECT player_id, count (*) as num
 	FROM players
@@ -25,31 +21,27 @@ CREATE VIEW count_players AS
 	;
 
 -- view: counts all matches played
--- ok; test with data entries
 CREATE VIEW matches_played AS
-    SELECT players.name, players.player_id, count (matches.player_id) as played
+    SELECT players.name, players.player_id, count (matches.match_id) as played
     FROM players LEFT JOIN matches
-	ON players.player_id = matches.player_id
+	ON players.player_id = matches.winner
     GROUP BY players.player_id
     ;
 
 -- view: counts all wins
--- ok; test with data entries
 CREATE VIEW wins AS
-	SELECT player_id, count (match_result) as wins
+	SELECT winner, count (*) as wins
 	FROM matches
-		WHERE match_result = 'win'
-	GROUP BY player_id
+	GROUP BY winner
 	ORDER BY wins DESC
 	;
 
 -- view: players standings
--- ok; test with data entries
 CREATE VIEW standings AS
 	SELECT matches_played.player_id, matches_played.name,
 	COALESCE (wins.wins,0) AS wins,
 	matches_played.played
 	FROM matches_played
-	LEFT JOIN wins ON matches_played.player_id = wins.player_id
+	LEFT JOIN wins ON matches_played.player_id = wins.winner 
 	ORDER BY wins DESC
 	;
